88e1a15e27ef9d0b3ced26a21809fa4440032d46
ben <bendixondavis@gmail.com>
2019-03-23 15:00:10 -0400

    got rotation of pieces working and collision detection with walls so can't rotate piece outside arena

--- tetris.js
+++ tetris.js
@@ -90,6 +90,49 @@ function playerMove(direction){
   }
 }
 
+/*rotates the piece and checks for collision with walls
+if collision it offsets the piece away from walls until
+no more collision*/
+function playerRotate(direction){
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, direction);
+  //tries offsetting the piece until it doesn't see a collision
+  while (collide(arena, player)){
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    /*if piece has been offset more than the length
+    of arena, reset piece back to original position
+    because it doesn't make sense*/
+    if (offset > player.matrix[0].length){
+      rotate(player.matrix, -direction);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+//rotates the given matrix the given direction
+function rotate(matrix, direction){
+  for (let y = 0; y < matrix.length; ++y){
+    for (let x = 0; x < y; ++x){
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
+  if (direction > 0){
+    matrix.forEach(row => row.reverse());
+  }
+  else{
+    matrix.reverse();
+  }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -107,17 +150,27 @@ function update(time = 0){
   requestAnimationFrame(update);
 }
 
-//this handles events created by arrow key preses
+//this handles events created by key preses
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37){
-    //if player presses left arrow key
+
     playerMove(-1);
   }
   else if (event.keyCode === 39){
+    //player presses right arrow key
     playerMove(1);
   }
   else if (event.keyCode === 40){
+    //player presses down arrow key
     playerDrop();
+  }
+  else if (event.keyCode === 81){
+    //player presses q to rotate left
+    playerRotate(-1);
+  }
+  else if (event.keyCode === 87){
+    //player presses w to rotate right
+    playerRotate(1);
   }
 });
 
